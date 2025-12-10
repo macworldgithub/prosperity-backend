948c82364cb32001190bf997964ec0c9b2816eae
jawwadkareem <karimjawwad09@gmail.com>
2025-12-10 17:14:54 +0500

    OTP swagger updated

--- src/otp/dto/get-otp.dto.ts
+++ src/otp/dto/get-otp.dto.ts
@@ -1,9 +1,19 @@
+// src/otp/dto/get-otp.dto.ts
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetOtpDto {
+  @ApiProperty({
+    description: 'Customer number',
+    example: '512345',
+  })
   @IsString()
   custNo: string;
 
+  @ApiProperty({
+    description: 'Phone number to receive OTP',
+    example: '0412121212',
+  })
   @IsString()
   destination: string;
 }
--- src/otp/dto/verify-otp.dto.ts
+++ src/otp/dto/verify-otp.dto.ts
@@ -1,9 +1,19 @@
+// src/otp/dto/verify-otp.dto.ts
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class VerifyOtpDto {
+  @ApiProperty({
+    description: 'OTP code received',
+    example: '483920',
+  })
   @IsString()
   code: string;
 
+  @ApiProperty({
+    description: 'Transaction ID returned from getOTP call',
+    example: 'ABC50111-DE56-440F-G5H2-8603684C123I',
+  })
   @IsString()
   transactionId: string;
 }
